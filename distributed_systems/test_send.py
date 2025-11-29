df29e5fecf7b0d79ca823b7c74032d157b11dfef
Nataliia Sokil <sokilnatalka@gmail.com>
2025-11-29 14:36:35 +0200

    updated logic

--- distributed_systems/test_send.py
+++ distributed_systems/test_send.py
@@ -5,16 +5,17 @@ import requests
 url = "http://localhost:42000/messages"
 
 def send(message):
-    payload = {"message": message, "write_concern": 3}
+    payload = {"message": message, "write_concern": 2}
     resp = requests.post(url, json=payload)
 
 threeads = [];
 for i in range(0, 100):
     message = "message %s %s" % (str(i), str(time.time()))
     print(message)
-    t = threading.Thread(target=send, args=(message,))
-    t.start()
-    threeads.append(t)
+    send(message)
+    # t = threading.Thread(target=send, args=(message,))
+    # t.start()
+    # threeads.append(t)
 
 
 for t in threeads:
